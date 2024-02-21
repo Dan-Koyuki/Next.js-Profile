3f33570adfcf58e17a00449203cce61a2218c5c0
Dan-Koyuki <dankoyuki398@gmail.com>
2024-02-21 17:02:11 +0700

    just trial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta
+        name="google-site-verification"
+        content="2xtToYMAGYHmQa4pCOMb8rKIkCjbUmdwKcvGzt7xL0A"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
